3093bcc60b2ba7dc05e2e10b537023f3804020f3
knkher <knkher@676b272c-f32c-0410-bc98-cf88ceb89b9e>
2007-04-08 03:17:05 +0000

    Payload now inline

--- trunk/Research/kq/base/Payload.cpp
+++ trunk/Research/kq/base/Payload.cpp
@@ -1,7 +1,7 @@
 #include "Payload.h"
-#include "stdio.h"
-#include "memory.h"
-#include "malloc.h"
+
+
+
 
 
 using namespace base;
@@ -14,7 +14,8 @@ Payload::Payload()
 
 Payload::Payload(Payload & payload)
 {
-		
+	init();
+	makeEqualTo(payload.m_pLocation, payload.m_uSize);		
 }
 
 Payload::~Payload()
@@ -25,80 +26,68 @@ Payload::~Payload()
 	}	
 }
 
-void Payload::clean(){
-	if(m_uSize && m_pLocation){
-		printf("\nFreeing %u bytes at %p", m_uSize, m_pLocation);
-		free(m_pLocation);
-	}	
-	init();
-}
-
-void Payload::init(){
-	m_pLocation = 0;
-	m_uSize = 0;	
-}
-
-PayloadSizeUnit Payload::getSize(){
-	return m_uSize;
-}
-void * Payload::getLocation(){
-	return m_pLocation;
-}
-
-
-
-//int Payload::deepCopy(Payload & payload){
-//	if(&payload == this){
-//		return iSuccess;
-//	}
-//	return deepCopy(payload.m_pLocation, payload.m_uSize);
+//void Payload::clean(){
+//	if(m_uSize && m_pLocation){
+//		printf("\nFreeing %u bytes at %p", m_uSize, m_pLocation);
+//		free(m_pLocation);
+//	}	
+//	init();
+//}
+//
+//void Payload::init(){
+//	m_pLocation = 0;
+//	m_uSize = 0;	
 //}
 
-int Payload::deepCopy(void * pLocation, PayloadSizeUnit uSize){
-	
-//	if(m_pLocation && m_uSize){
-//		printf("\nbase::Payload::_deepCopy::Warning: Data left floating.");
+//PayloadSizeUnit Payload::getSize(){
+//	return m_uSize;
+//}
+//void * Payload::getLocation(){
+//	return m_pLocation;
+//}
+
+
+
+//int Payload::deepCopy(void * pLocation, PayloadSizeUnit uSize){
+//	clean();
+//	
+//	m_pLocation = malloc(uSize);
+//	if(!m_pLocation){
+//		return iFailure;
 //	}
+//	m_uSize = uSize;	
+//	memcpy(m_pLocation, pLocation, m_uSize);	
+//	return iSuccess;
+//}
 
-	clean();
-	
-	m_pLocation = malloc(uSize);
-	if(!m_pLocation){
-		return iFailure;
-	}
-	m_uSize = uSize;	
-	memcpy(m_pLocation, pLocation, m_uSize);	
-	return iSuccess;
-}
+//
+//bool Payload::isEqualTo(void * pLocation, PayloadSizeUnit uSize){
+//	
+//	//If not of the same size then definitely not equal
+//	if(m_uSize != uSize){
+//		return false;
+//	}
+//	//Assume same size below
+//	
+//	//Now if same location then surely the same
+//	if(m_pLocation == pLocation){
+//		return true;
+//	}
+//	
+//	if(memcmp(m_pLocation, pLocation, m_uSize) == 0){
+//		return true;
+//	}
+//	
+//	return false;
+//}
 
-
-bool Payload::isEqualTo(void * pLocation, PayloadSizeUnit uSize){
-	
-	//If not of the same size then definitely not equal
-	if(m_uSize != uSize){
-		return false;
-	}
-	//Assume same size below
-	
-	//Now if same location then surely the same
-	if(m_pLocation == pLocation){
-		return true;
-	}
-	
-	if(memcmp(m_pLocation, pLocation, m_uSize) == 0){
-		return true;
-	}
-	
-	return false;
-}
-
-bool Payload::operator==(Payload & payload){
-	
-	if(this == &payload){
-		return true;
-	}
-	return isEqualTo(payload.m_pLocation, payload.m_uSize);	
-}
+//bool Payload::operator==(Payload & payload){
+//	
+//	if(this == &payload){
+//		return true;
+//	}
+//	return isEqualTo(payload.m_pLocation, payload.m_uSize);	
+//}
 
 Payload & Payload::operator= (Payload & payload){
 	deepCopy(payload.m_pLocation, payload.m_uSize);
--- trunk/Research/kq/base/Payload.h
+++ trunk/Research/kq/base/Payload.h
@@ -2,6 +2,10 @@
 #define PAYLOAD_H_
 
 #include "dataTypes.h"
+#include "malloc.h"
+#include "memory.h"
+#include "stdio.h"
+
 namespace base{
 	
 	typedef unsigned int PayloadSizeUnit;
@@ -12,22 +16,68 @@ namespace base{
 		
 		PayloadSizeUnit m_uSize;
 		
-		int deepCopy(void * pLocation, PayloadSizeUnit uSize);
+		inline int deepCopy(void * pLocation, PayloadSizeUnit uSize){
+			clean();
+	
+			m_pLocation = malloc(uSize);
+			if(!m_pLocation){
+				return iFailure;
+			}
+			m_uSize = uSize;	
+			memcpy(m_pLocation, pLocation, m_uSize);	
+			return iSuccess;
+		}
 		
 	public:	
 		Payload();
 		Payload(Payload & payload);
 		virtual ~Payload();
-		void clean();
-		void init();
 		
-		PayloadSizeUnit getSize();
-		void * getLocation();	
-		//int deepCopy(Payload &pPayload);
+		inline void clean(){
+			if(m_uSize && m_pLocation){
+				printf("\nFreeing %u bytes at %p", m_uSize, m_pLocation);
+				free(m_pLocation);
+			}	
+			init();
+		}
 		
+		inline void init(){
+			m_pLocation = 0;
+			m_uSize = 0;	
+		}
 		
-		bool operator==(Payload &pPayload);
-		bool isEqualTo(void * pLocation, PayloadSizeUnit uSize);
+		inline PayloadSizeUnit getSize(){
+			return m_uSize;
+		}
+		inline void * getLocation(){
+			return m_pLocation;	
+		}		
+				
+		bool operator==(Payload & payload){	
+			if(this == &payload){
+				return true;
+			}
+			return isEqualTo(payload.m_pLocation, payload.m_uSize);	
+		}
+		
+		inline bool isEqualTo(void * pLocation, PayloadSizeUnit uSize){
+			//If not of the same size then definitely not equal
+			if(m_uSize != uSize){
+				return false;
+			}
+			//Assume same size below
+			
+			//Now if same location then surely the same
+			if(m_pLocation == pLocation){
+				return true;
+			}
+			
+			if(memcmp(m_pLocation, pLocation, m_uSize) == 0){
+				return true;
+			}
+			
+			return false;
+		}
 		
 		Payload & operator= (Payload & payload);
 		Payload & makeEqualTo(void * pLocation, PayloadSizeUnit uSize);		 
